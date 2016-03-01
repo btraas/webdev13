ce59236d0e67e6d6153e6cfa3e5e047e3e4c21d7
Brayden Traas <btraas@gmail.com>
2016-02-29 19:55:46 -0800

    simplifying

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -11,23 +11,12 @@ if( $_POST['payload'] ) {
 	
 
 	copy('sync_in_progress.html', 'index.html');
-	sleep(1);
 
-	$msg  = shell_exec( $GIT_PATH.'git fetch origin master' );
-	$msg2 = shell_exec( $GIT_PATH.'git reset --hard HEAD' ); 
-	$msg3 = shell_exec( $GIT_PATH.'git merge -s recursive -X theirs origin/master' );
+	echo shell_exec( $GIT_PATH.'git fetch origin master' );
+	echo shell_exec( $GIT_PATH.'git reset --hard HEAD' ); 
+	echo shell_exec( $GIT_PATH.'git merge -s recursive -X theirs origin/master' );
 
-	if($msg3 == 'Already up-to-date.') 
-	{
-		echo "$msg\n$msg2\nTrying again...\n";
-		sleep(2);
-		$msg  .= shell_exec( $GIT_PATH.'git fetch origin master' );
-		$msg2 = shell_exec( $GIT_PATH.'git reset --hard HEAD' );
-		$msg3 = shell_exec( $GIT_PATH.'git merge -s recursive -X theirs origin/master' );
-
-	}
 }
 
-echo "$msg\n$msg2\n$msg3";
 
 ?>

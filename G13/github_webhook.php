88070eefca883d13d54f537e65cd982d08ef99cf
Brayden Traas <btraas@gmail.com>
2016-02-29 19:12:17 -0800

    Github Webhook outputx

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -7,14 +7,16 @@ $GIT_PATH = "C:/cygwin64/bin/";
 
 $_POST['payload'] = 'hi';
 
-if( $_POST['payload'] ) {
+$msg = "";
 
+if( $_POST['payload'] ) {
+	
 
 	copy('sync_in_progress.html', 'index.html');
-	shell_exec( $GIT_PATH.'git fetch origin master' );
-	shell_exec( $GIT_PATH.'git reset --hard HEAD & '.$GIT_PATH.'git merge -s recursive -X theirs origin/master' );
+	$msg .= shell_exec( $GIT_PATH.'git fetch origin master' );
+	$msg .= " \n".shell_exec( $GIT_PATH.'git reset --hard HEAD & '.$GIT_PATH.'git merge -s recursive -X theirs origin/master' );
 }
 
-echo "Files pulled from Github";
+echo $msg;
 
 ?>

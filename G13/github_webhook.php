d356ae2c8b87aa681e85b13361e4709925aaa773
Brayden Traas <btraas@gmail.com>
2016-02-29 19:38:15 -0800

    This should work now

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -7,26 +7,25 @@ $GIT_PATH = "C:/cygwin64/bin/";
 
 $_POST['payload'] = 'hi';
 
-$msg = "";
-$msg2 = "";
-
 if( $_POST['payload'] ) {
 	
 
 	copy('sync_in_progress.html', 'index.html');
-	$msg  .= shell_exec( $GIT_PATH.'git fetch origin master' );
-	$msg2 .= shell_exec( $GIT_PATH.'git reset --hard HEAD & '.$GIT_PATH.'git merge -s recursive -X theirs origin/master' );
+	$msg  = shell_exec( $GIT_PATH.'git fetch origin master' );
+	$msg2 = shell_exec( $GIT_PATH.'git reset --hard HEAD' ); 
+	$msg3 = shell_exec( $GIT_PATH.'git merge -s recursive -X theirs origin/master' );
 
 	if($msg2 == 'Already up-to-date.') 
 	{
 		echo "$msg\n$msg2\nTrying again...\n";
 		sleep(2);
 		$msg  .= shell_exec( $GIT_PATH.'git fetch origin master' );
-		$msg2 .= shell_exec( $GIT_PATH.'git reset --hard HEAD & '.$GIT_PATH.'git merge -s recursive -X theirs origin/master' );
+		$msg2 = shell_exec( $GIT_PATH.'git reset --hard HEAD' );
+		$msg3 = shell_exec( $GIT_PATH.'git merge -s recursive -X theirs origin/master' );
 
 	}
 }
 
-echo "$msg\n$msg2";
+echo "$msg\n$msg2\n$msg3";
 
 ?>

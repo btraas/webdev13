79118e62d02dfc6c5bcc6463dfb8eb45d8047ec8
Brayden Traas <btraas@gmail.com>
2016-02-29 20:29:39 -0800

    test again

--- G13/pull.sh
+++ G13/pull.sh
@@ -1,12 +1,14 @@
 FOLDER='/cygdrive/d/webdev13/G13/'
 cd $FOLDER
-cp sync_in_progress.html index.html
+`cp sync_in_progress.html index.html`
 
 echo "pull.sh run" >itworked.txt
+#exit
+
+echo `git fetch`
+echo `git reset --hard HEAD`
+echo `git merge -s recursive -X theirs origin/master`
 exit
 
-git fetch
-git reset --hard HEAD
-git merge -s recursive -X theirs origin/master
 #git pull
 

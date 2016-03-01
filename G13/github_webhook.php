e8b94039c93630d14ed0bfc083c86950e392301b
Brayden Traas <btraas@gmail.com>
2016-02-29 19:41:43 -0800

    This should do it

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -15,7 +15,7 @@ if( $_POST['payload'] ) {
 	$msg2 = shell_exec( $GIT_PATH.'git reset --hard HEAD' ); 
 	$msg3 = shell_exec( $GIT_PATH.'git merge -s recursive -X theirs origin/master' );
 
-	if($msg2 == 'Already up-to-date.') 
+	if($msg3 == 'Already up-to-date.') 
 	{
 		echo "$msg\n$msg2\nTrying again...\n";
 		sleep(2);

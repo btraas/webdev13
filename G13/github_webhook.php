36b5d1714fb91a8d7fb48b68270c946ba39527b7
Brayden Traas <btraas@gmail.com>
2016-02-29 19:19:11 -0800

    Added a tryagain

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -8,15 +8,25 @@ $GIT_PATH = "C:/cygwin64/bin/";
 $_POST['payload'] = 'hi';
 
 $msg = "";
+$msg2 = "";
 
 if( $_POST['payload'] ) {
 	
 
 	copy('sync_in_progress.html', 'index.html');
-	$msg .= shell_exec( $GIT_PATH.'git fetch origin master' );
-	$msg .= " \n".shell_exec( $GIT_PATH.'git reset --hard HEAD & '.$GIT_PATH.'git merge -s recursive -X theirs origin/master' );
+	$msg  .= shell_exec( $GIT_PATH.'git fetch origin master' );
+	$msg2 .= shell_exec( $GIT_PATH.'git reset --hard HEAD & '.$GIT_PATH.'git merge -s recursive -X theirs origin/master' );
+
+	if($msg2 == 'Already up-to-date.') 
+	{
+		echo "$msg\n$msg2\nTrying again...\n";
+		sleep(2);
+		$msg  .= shell_exec( $GIT_PATH.'git fetch origin master' );
+		$msg2 .= shell_exec( $GIT_PATH.'git reset --hard HEAD & '.$GIT_PATH.'git merge -s recursive -X theirs origin/master' );
+
+	}
 }
 
-echo $msg;
+echo "$msg\n$msg2";
 
 ?>

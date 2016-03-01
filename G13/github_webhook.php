21cb1f6a371a1e2ecd2c9e6fa7badfec596720ed
Brayden Traas <btraas@gmail.com>
2016-02-29 18:03:46 -0800

    Due to the nature of this script, I must push it before running to test :)

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -3,16 +3,16 @@
 // Git auto-pull script called by github. Provided by gist.github.com/cowboy/619858
 
 
-$GIT_PATH = "C:\\cygwin64\\bin\\";
+$GIT_PATH = "C:/cygwin64/bin/";
+
+$_POST['payload'] = 'hi';
 
 if( $_POST['payload'] ) {
 
 
-	copy('index.html', 'index.html.bak');
 	copy('sync_in_progress.html', 'index.html');
-	shell_exec( $GIT_PATH.' git fetch origin master' );
-	shell_exec( $GIT_PATH.' git merge -s recursive -X theirs origin/master' );
-
+	shell_exec( $GIT_PATH.'git fetch origin master' );
+	shell_exec( $GIT_PATH.'git reset --hard HEAD & '.$GIT_PATH.'git merge -s recursive -X theirs origin/master' );
 }
 
 ?>

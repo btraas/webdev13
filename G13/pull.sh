46abd436f98ddcedca9589787e1c81626ca73673
Brayden Traas <btraas@gmail.com>
2016-02-29 20:36:47 -0800

    testingx

--- G13/pull.sh
+++ G13/pull.sh
@@ -1,9 +1,13 @@
-FOLDER='/cygdrive/d/webdev13/G13/'
+FOLDER='/cygdrive/d/webdev13/G13'
 cd $FOLDER
-`cp sync_in_progress.html index.html`
+cp sync_in_progress.html index.html
 
 echo "pull.sh run" >itworked.txt
 #exit
+
+git pull
+
+exit
 
 echo `git fetch`
 echo `git reset --hard HEAD`

0c53ea798c1a0a74815b936811c79350d0f4e3f7
Brayden Traas <btraas@gmail.com>
2016-03-08 15:10:03 -0800

    Better formatting

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -1,14 +1,14 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
-if ($_REQUEST['pass'] == getenv("GITHUB_PASS")) {
+if (@$_REQUEST['pass'] == getenv("GITHUB_PASS")) {
     echo nl2br(shell_exec( 'cd /var/www/webdev13/ && sudo git reset --hard HEAD && sudo git pull' ));
     exit();
 }
 
 // else display info
 
-
+echo "<h1>Last 2 commits:</h1><br /><br />";
 echo nl2br(shell_exec( 'cd /var/www/webdev13/ && sudo git log -p -2' ));
 
 ?>

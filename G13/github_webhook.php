fdb0b9f89ce782e7b8b69bf90647812d92f53473
Brayden Traas <btraas@gmail.com>
2016-03-08 14:28:34 -0800

    fixed bug

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -1,7 +1,7 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
-if ( $_REQUEST['payload'] && $_REQUEST['pass'] == getenv("GITHUB_PASS")) {
+if ($_REQUEST['pass'] == getenv("GITHUB_PASS")) {
     echo nl2br(shell_exec( 'cd /var/www/webdev13/ && sudo git reset --hard HEAD && sudo git pull' ));
     exit();
 }

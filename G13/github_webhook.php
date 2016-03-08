3f2cbf7e83149a9ce73bb45aacc5b900b0f7b186
Brayden Traas <btraas@gmail.com>
2016-03-08 13:15:40 -0800

    test

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -1,7 +1,12 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/webdev13/ && sudo git reset --hard HEAD && sudo git pull' );
+if ( $_REQUEST['payload'] ) {
+    echo shell_exec( 'cd /var/www/webdev13/ && sudo git reset --hard HEAD && sudo git pull' );
+    exit();
 }
+
+echo "Invalid format!";
+
 ?>
+
 

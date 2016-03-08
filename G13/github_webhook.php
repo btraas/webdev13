91729b433b3be088b2a9a0da36a71cce28a119b4
Brayden Traas <btraas@gmail.com>
2016-03-08 13:58:46 -0800

    update to show what to compare incoming address to

--- G13/github_webhook.php
+++ G13/github_webhook.php
@@ -1,6 +1,11 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_REQUEST['payload'] ) {
+
+$ip = $_SERVER['REMOTE_ADDR'];
+
+echo gethostbyaddr($ip);
+
+if ( $_REQUEST['payload']) {
     echo nl2br(shell_exec( 'cd /var/www/webdev13/ && sudo git reset --hard HEAD && sudo git pull' ));
     exit();
 }

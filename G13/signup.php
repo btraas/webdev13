44833c8265a0e915fe1d76edf08c2af47fee104d
Ian <bcitipark15@gmail.com>
2016-03-28 05:45:30 -0700

    Removed echo messages

--- G13/signup.php
+++ G13/signup.php
@@ -11,18 +11,6 @@
 	//Validation error flag
 	$errflag = false;
 	
-	/*//Connect to mysql server
-	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-	if(!$link) {
-		die('Failed to connect to server: ' . mysql_error());
-	}
-	
-	//Select database
-	$db = mysql_select_db(DB_DATABASE);
-	if(!$db) {
-		die("Unable to select database");
-	}*/
-	
 	//Function to sanitize values received from the form. Prevents SQL injection
 	function clean($str) {
 		$str = @trim($str);

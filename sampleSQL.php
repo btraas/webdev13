7c1046b4bb75766e65294ea68a8651db937b0e1f
Brayden Traas <btraas@gmail.com>
2016-03-27 21:33:58 -0700

    Removed order HTML pages. These have all been moved to a single PHP page. Apache redirects old links to the new pages anyways.

--- sampleSQL.php
+++ sampleSQL.php
@@ -1,14 +1,25 @@
 <?php
-$link = new mysqli("bcitdevcom.ipagemysql.com",
-"comp15362014", "2014-1536")
+
+$link = new mysqli(		"localhost", 	// Server (localhost = this server)
+						"webdev13", 	// Username
+						"c1536withBen",	// Password
+						"webdev13",		// Database name
+					)
 or die("Could not connect: " . $link->connect_error());
-print ("Connected successfully<br>");
-$link->select_db("1536forum");
-$query = "SELECT * FROM members";
+
+print ("Connected successfully <br \>");
+
+$query = "SELECT * FROM products";
 $result = $link->query($query);
-while ($row = $result->fetch_assoc()) { // get a row
-$lastName = $row['lastname']; // get last name
-$firstName = $row['firstname']; // get first name
-echo $lastName . ", " . $firstName . "<br>"; }
+
+while ($row = $result->fetch_assoc()) 	// get each row
+{ 
+	$name = $row['name']; 		// get this row's 'name' column
+	$price= $row['price']; 	// get this row's 'price' column
+
+	echo $name . ", " . $price . "<br>"; 
+}
+
+
 $link->close();
 ?>

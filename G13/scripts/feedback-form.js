67b75ad2663675836bc74a69df8ec4f7c1eea879
Davin-Deol <davindeol@gmail.com>
2016-04-05 15:09:14 -0700

    Tried && instead of || for clear function

--- G13/scripts/feedback-form.js
+++ G13/scripts/feedback-form.js
@@ -31,7 +31,7 @@ function clear() {
 	var details = document.getElementById("details").value="";
 	var subject = document.getElementById("subject").value="";
 
-	if (details.value != "" || subject.value !="")
+	if (details.value != "" && subject.value != "")
 		details.value = "";
 		subject.value = "";
 }
